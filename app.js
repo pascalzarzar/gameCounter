ff4a5987ae4ab114a642d07c555dd6d38c836d4a
Pascal Zarzar <pascal.zarzar@gmail.com>
2021-06-16 18:45:01 -0400

    Refactoring of the app.js file

--- app.js
+++ app.js
@@ -18,57 +18,69 @@ let gamesCount = 1;
 let player1Score = 0;
 let player2Score = 0;
 
-//Escuchamos los cambios que se ejecutan en el select y reseteamos los puntajes
-gamesSelector.addEventListener('change', () => {
-    gamesCount = parseInt(gamesSelector.value);
+const disableButtons = () => {
+    player1Point.disabled = true;
+    player2Point.disabled = true;
+};
+
+const verifyWinner = (player1, player2) => {
+    if (gamesCount === player1) {
+        result.innerHTML = 'Player 1 is the winner!';
+        player1DomScore.classList.add('win');
+        player2DomScore.classList.add('lose');
+        disableButtons();
+    } else if (gamesCount === player2) {
+        result.innerHTML = 'Player 2 is the winner!';
+        player2DomScore.classList.add('win');
+        player1DomScore.classList.add('lose');
+        disableButtons();
+    }  
+};
+
+const resetValues = () => {
+    result.innerHTML = '';
     player1DomScore.innerHTML = 0;
     player2DomScore.innerHTML = 0;
     player1Score = 0;
     player2Score = 0;
+    player1Point.disabled = false;
+    player2Point.disabled = false;
+};
+
+const resetGame = (player1,player2) => {
+    if (gamesCount === player1){
+        player1DomScore.classList.remove('win');
+        player2DomScore.classList.remove('lose');
+    } else if (gamesCount === player2) {
+        player2DomScore.classList.remove('win');
+        player1DomScore.classList.remove('lose');
+    }
+    gamesCount = 1;
+    gamesSelector.value = 1;
+    resetValues();
+};
+
+//Escuchamos los cambios que se ejecutan en el select y reseteamos los puntajes
+gamesSelector.addEventListener('change', () => {
+    gamesCount = parseInt(gamesSelector.value);
+    resetValues();
 });
 
 //Escuchamos el botón que le asigna puntaje al player 1 y validamos si se cumple la condición para ganar el juego.
 player1Point.addEventListener('click', () => {
     player1DomScore.innerHTML++;
-    player1Score++
-    if (gamesCount === player1Score) {
-        result.innerHTML = 'Player 1 is the winner!'
-        player1DomScore.classList.add('win');
-        player2DomScore.classList.add('lose');
-        player1Point.disabled = true;
-        player2Point.disabled = true;
-    }
+    player1Score++;
+    verifyWinner(player1Score, player2Score);
 });
 
 //Escuchamos el botón que le asigna puntaje al player 2 y validamos si se cumple la condición para ganar el juego.
 player2Point.addEventListener('click', () => {
     player2DomScore.innerHTML++;
-    player2Score++
-    if (gamesCount === player2Score) {
-        result.innerHTML = 'Player 2 is the winner!'
-        player2DomScore.classList.add('win');
-        player1DomScore.classList.add('lose');
-        player1Point.disabled = true;
-        player2Point.disabled = true;
-    }
+    player2Score++;
+    verifyWinner(player1Score, player2Score);
 });
 
 //Escuchamos el botón para resetear el contador y todas las variables implicadas.
 reset.addEventListener('click', () => {
-    if (gamesCount === player1Score){
-        player1DomScore.classList.remove('win');
-        player2DomScore.classList.remove('lose');
-    } else if (gamesCount === player2Score) {
-        player2DomScore.classList.remove('win');
-        player1DomScore.classList.remove('lose');
-    }
-    result.innerHTML = ''
-    player1DomScore.innerHTML = 0;
-    player2DomScore.innerHTML = 0;
-    player1Score = 0;
-    player2Score = 0;
-    gamesCount = 1;
-    gamesSelector.value = 1;
-    player1Point.disabled = false;
-    player2Point.disabled = false;
+    resetGame(player1Score,player2Score);
 });
